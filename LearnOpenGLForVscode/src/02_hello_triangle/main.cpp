c61b8d8f97c512dd6b4caed16c46f603c0a99d74
chengrong.liu <chengrong.liu@i-tudou.com>
2022-03-29 18:41:02 +0800

    你好三角形

---
+++ LearnOpenGLForVscode/src/02_hello_triangle/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+float vertices[] = {
+  -0.5f, -0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f,
+  0.0f, 0.5f, 0.0f
+}
+unsigned int VBO;
+glGenBuffers(1, &VBO);
+glBindBuffer(GL_ARRAY_BUFFER, VBO);
+glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW)
+
